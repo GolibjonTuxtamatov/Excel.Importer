27600a30c7d4a90fc592a4ca5a972e6ce1ebe859
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-10-19 22:01:43 +0500

    POC:Create Applicant Model And Migrate

---
+++ Excel.Importer/Brokers/Storages/StorageBroker.Applicants.cs
@@ -0,0 +1,16 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using Excel.Importer.Models.Foundations.Applicants;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Excel.Importer.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Applicant> Applicants { get; set; }
+    }
+}
