b7d368ce98e63f88525d08ab584b73b9c6bedcf5
99894 <99894@DESKTOP-K351R76>
2023-10-19 20:45:03 +0500

    POC: Code Rub

--- Excel.Importer/Models/Foundations/ExternalApplicants/ExternalApplicant.cs
+++ Excel.Importer/Models/Foundations/ExternalApplicants/ExternalApplicant.cs
@@ -1,6 +1,16 @@
-﻿namespace Excel.Importer.Models.Foundations.ExternalApplicants
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+namespace Excel.Importer.Models.Foundations.ExternalApplicants
 {
     public class ExternalApplicant
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string GroupName { get; set; }
     }
 }
