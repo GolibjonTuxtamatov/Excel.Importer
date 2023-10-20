7821b31576e6d90be7fc02f320da751b2a070690
99894 <99894@DESKTOP-K351R76>
2023-10-20 17:10:19 +0500

    POC: Add Group

---
+++ Excel.Importer/Services/Proccessings/Groups/IGroupProccessingService.cs
@@ -0,0 +1,15 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Threading.Tasks;
+using Excel.Importer.Models.Foundations.Groups;
+
+namespace Excel.Importer.Services.Proccessings.Groups
+{
+    public interface IGroupProccessingService
+    {
+        ValueTask<Group> AddGroupAsync(Group group);
+    }
+}
