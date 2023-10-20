14da0a019a2b923f06d4b7d71869683f80145ada
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-10-20 15:53:07 +0500

    POC: Add Applicant

---
+++ Excel.Importer/Services/Foundations/Applicants/ApplicantService.cs
@@ -0,0 +1,26 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Threading.Tasks;
+using Excel.Importer.Brokers.Storages;
+using Excel.Importer.Models.Foundations.Applicants;
+
+namespace Excel.Importer.Services.Foundations.Applicants
+{
+    public class ApplicantService : IApplicantService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public ApplicantService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public ValueTask<Applicant> AddApplicantAsync(Applicant applicant)
+        {
+            return this.storageBroker.InsertApplicantAsync(applicant);
+        }
+    }
+}
