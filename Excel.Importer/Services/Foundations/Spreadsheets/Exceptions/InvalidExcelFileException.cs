0681e4f669c8e2394ecb86e6a19ebb200fd42840
99894 <99894@DESKTOP-K351R76>
2023-10-21 20:04:26 +0500

    FOUNDATIONS: Excel File IsValid

---
+++ Excel.Importer/Services/Foundations/Spreadsheets/Exceptions/InvalidExcelFileException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xeptions;
+
+namespace Excel.Importer.Services.Foundations.Spreadsheets.Exceptions
+{
+    public class InvalidExcelFileException : Xeption
+    {
+        public InvalidExcelFileException(Exception innerException)
+            :base("Excel file is invalid",
+                 innerException)
+        { }
+    }
+}
--- Excel.Importer/Services/Foundations/Spreadsheets/SpreadsheetService.cs
+++ Excel.Importer/Services/Foundations/Spreadsheets/SpreadsheetService.cs
@@ -1,23 +1,42 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Excel.Importer.Brokers.Loggings;
 using Excel.Importer.Brokers.Spreadsheets;
 using Excel.Importer.Models.Foundations.ExternalApplicants;
+using Excel.Importer.Services.Foundations.Spreadsheets.Exceptions;
 
 namespace Excel.Importer.Services.Foundations.Spreadsheets
 {
     public class SpreadsheetService : ISpreadsheetService
     {
         private readonly ISpreadsheetBroker spreadsheetBroker;
+        private readonly ILoggingBroker loggingBroker;
 
-        public SpreadsheetService(ISpreadsheetBroker spreadsheetBroker)
+        public SpreadsheetService(ISpreadsheetBroker spreadsheetBroker, ILoggingBroker loggingBroker)
         {
             this.spreadsheetBroker = spreadsheetBroker;
+            this.loggingBroker = loggingBroker;
         }
 
         public async ValueTask<List<ExternalApplicant>> ImportExternalApplicantAsync(MemoryStream spreasheet)
         {
-            return await this.spreadsheetBroker.ReadSpreadsheetAsync(spreasheet);
+            try
+            {
+                return await this.spreadsheetBroker.ReadSpreadsheetAsync(spreasheet);
+            }
+            catch (InvalidDataException invalidDataException)
+            {
+                var invalidExcelFileException =
+                    new InvalidExcelFileException(invalidDataException);
+
+                var excelFileValidationException =
+                    new ExcelFileValidationException(invalidExcelFileException);
+
+                this.loggingBroker.LogError(excelFileValidationException);
+
+                throw excelFileValidationException;
+            }
         }
     }
 }
