c9e0fdfac0caca4aeea7a092f85f6fbe5f776135
99894 <99894@DESKTOP-K351R76>
2023-10-21 07:38:03 +0500

    POC: Proccessing Add Aplicant

---
+++ Excel.Importer/Services/Proccessings/Applicants/ApplicantProccessingService.cs
@@ -0,0 +1,26 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Threading.Tasks;
+using Excel.Importer.Models.Foundations.Applicants;
+using Excel.Importer.Services.Foundations.Applicants;
+
+namespace Excel.Importer.Services.Proccessings.Applicants
+{
+    public class ApplicantProccessingService : IApplicantProccessingService
+    {
+        private readonly IApplicantService applicantService;
+
+        public ApplicantProccessingService(IApplicantService applicantService)
+        {
+            this.applicantService = applicantService;
+        }
+
+        public ValueTask<Applicant> AddApplicantAsync(Applicant applicant)
+        {
+            return this.applicantService.AddApplicantAsync(applicant);
+        }
+    }
+}
