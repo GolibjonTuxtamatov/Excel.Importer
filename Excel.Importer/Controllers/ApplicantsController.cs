2bf86bd325f3ab3943ee51ce76ca18230d1b8db4
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-10-21 15:16:04 +0500

    POC: Post Applicant

---
+++ Excel.Importer/Controllers/ApplicantsController.cs
@@ -0,0 +1,31 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Threading.Tasks;
+using Excel.Importer.Models.Foundations.Applicants;
+using Excel.Importer.Services.Orchestrations.Applicants;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Excel.Importer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApplicantsController : RESTFulController
+    {
+        private IApplicantOrchestrationService applicantOrchestrationService;
+
+        public ApplicantsController(IApplicantOrchestrationService applicantOrchestrationService)
+        {
+            this.applicantOrchestrationService = applicantOrchestrationService;
+        }
+
+        [HttpPost]
+        public async ValueTask<Applicant> PostGroupAsync(Applicant applicant)
+        {
+            return await this.applicantOrchestrationService.AddApplicantAsync(applicant);
+        }
+    }
+}
