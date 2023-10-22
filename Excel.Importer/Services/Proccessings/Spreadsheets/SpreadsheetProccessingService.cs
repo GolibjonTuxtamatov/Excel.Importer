72b60cf7d74daa4a1a3c66754d5ecfedbc46fa09
99894 <99894@DESKTOP-K351R76>
2023-10-22 17:03:51 +0500

    PROCCESSINGS: Excel Invalid Validation

--- Excel.Importer/Services/Proccessings/Spreadsheets/SpreadsheetProccessingService.cs
+++ Excel.Importer/Services/Proccessings/Spreadsheets/SpreadsheetProccessingService.cs
@@ -6,23 +6,46 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Excel.Importer.Brokers.Loggings;
 using Excel.Importer.Models.Foundations.ExternalApplicants;
 using Excel.Importer.Services.Foundations.Spreadsheets;
+using Excel.Importer.Services.Foundations.Spreadsheets.Exceptions;
+using Excel.Importer.Services.Proccessings.Spreadsheets.Exceptions;
+using Xeptions;
 
 namespace Excel.Importer.Services.Proccessings.Spreadsheets
 {
     public class SpreadsheetProccessingService : ISpreadsheetProccessingService
     {
         private readonly ISpreadsheetService spreadsheetService;
+        private readonly ILoggingBroker loggingBroker;
 
-        public SpreadsheetProccessingService(ISpreadsheetService spreadsheetService)
+        public SpreadsheetProccessingService(ISpreadsheetService spreadsheetService,ILoggingBroker loggingBroker)
         {
             this.spreadsheetService = spreadsheetService;
+            this.loggingBroker = loggingBroker;
         }
 
-        public ValueTask<List<ExternalApplicant>> GetExternalApplicantsAsync(MemoryStream spreadsheet)
+        public async ValueTask<List<ExternalApplicant>> GetExternalApplicantsAsync(MemoryStream spreadsheet)
         {
-            return this.spreadsheetService.ImportExternalApplicantAsync(spreadsheet);
+            try
+            {
+                return await this.spreadsheetService.ImportExternalApplicantAsync(spreadsheet);
+            }
+            catch (ExcelFileValidationException excelFileValidationException)
+            {
+                throw CreateAndLogProccessingValidationException(excelFileValidationException);
+            }
+        }
+
+        private ExcelFileProccessingValidationException CreateAndLogProccessingValidationException(Xeption exception)
+        {
+            var excelFileProccessingValidationException =
+                new ExcelFileProccessingValidationException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(excelFileProccessingValidationException);
+
+            return excelFileProccessingValidationException;
         }
     }
 }
