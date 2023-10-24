93345318284df2fbfa531a09a75e85f9ca924186
99894 <99894@DESKTOP-K351R76>
2023-10-24 11:06:42 +0500

    PROCCESSINGS: Validation Exception

---
+++ Excel.Importer/Services/Proccessings/Groups/Exceptions/GroupProccessingValidationException.cs
@@ -0,0 +1,17 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using Xeptions;
+
+namespace Excel.Importer.Services.Proccessings.Groups.Exceptions
+{
+    public class GroupProccessingValidationException: Xeption
+    {
+        public GroupProccessingValidationException(Xeption innerException)
+            :base("Group proccessing validation error occured, fix the error and try again",
+                 innerException)
+        { }
+    }
+}
---
+++ Excel.Importer/Services/Proccessings/Groups/GroupProccessingService.Exceptions.cs
@@ -0,0 +1,40 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Threading.Tasks;
+using Excel.Importer.Models.Foundations.Groups;
+using Excel.Importer.Services.Foundations.Groups.Exceptions;
+using Excel.Importer.Services.Proccessings.Groups.Exceptions;
+using Xeptions;
+
+namespace Excel.Importer.Services.Proccessings.Groups
+{
+    public partial class GroupProccessingService
+    {
+        private delegate ValueTask<Group> ReturningGroupFunction();
+
+        private async ValueTask<Group> TryCatch(ReturningGroupFunction returningGroupFunction)
+        {
+            try
+            {
+                return await returningGroupFunction();
+            }
+            catch (GroupValidationException groupValidationException)
+            {
+                throw CreateAndLogProccessingValidationException(groupValidationException);
+            }
+        }
+
+        private GroupProccessingValidationException CreateAndLogProccessingValidationException(Xeption exception)
+        {
+            var groupProccessingValidationException =
+                new GroupProccessingValidationException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(groupProccessingValidationException);
+
+            return groupProccessingValidationException;
+        }
+    }
+}
