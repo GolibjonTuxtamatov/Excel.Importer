0b748ba62c2790d0dc35cd6a5380e96f06936fd4
99894 <99894@DESKTOP-K351R76>
2023-10-24 19:28:32 +0500

    ORCHESTRATIONS: Validation Exception

---
+++ Excel.Importer/Services/Orchestrations/Groups/GroupOrchestrationService.Exceptions.cs
@@ -0,0 +1,40 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Threading.Tasks;
+using Excel.Importer.Models.Foundations.Groups;
+using Excel.Importer.Services.Orchestrations.Groups.Exceptions;
+using Excel.Importer.Services.Proccessings.Groups.Exceptions;
+using Xeptions;
+
+namespace Excel.Importer.Services.Orchestrations.Groups
+{
+    public partial class GroupOrchestrationService
+    {
+        private delegate ValueTask<Group> ReturningGroupFunction();
+
+        private async ValueTask<Group> TryCatch(ReturningGroupFunction returningGroupFunction)
+        {
+            try
+            {
+                return await returningGroupFunction();
+            }
+            catch (GroupProccessingValidationException groupProccessingValidationException)
+            {
+                throw CreateAndLogOrchetrationValidationException(groupProccessingValidationException);
+            }
+        }
+
+        private GroupOrchestrationValidationException CreateAndLogOrchetrationValidationException(Xeption exception)
+        {
+            var groupOrchestrationValidationException =
+                new GroupOrchestrationValidationException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(groupOrchestrationValidationException);
+
+            return groupOrchestrationValidationException;
+        }
+    }
+}
--- Excel.Importer/Services/Orchestrations/Groups/GroupOrchestrationService.cs
+++ Excel.Importer/Services/Orchestrations/Groups/GroupOrchestrationService.cs
@@ -5,24 +5,30 @@
 
 using System.Linq;
 using System.Threading.Tasks;
+using Excel.Importer.Brokers.Loggings;
 using Excel.Importer.Models.Foundations.Groups;
 using Excel.Importer.Services.Proccessings.Groups;
 
 namespace Excel.Importer.Services.Orchestrations.Groups
 {
-    public class GroupOrchestrationService : IGroupOrchestrationService
+    public partial class GroupOrchestrationService : IGroupOrchestrationService
     {
         private readonly IGroupProccessingService groupProccessingService;
+        private readonly ILoggingBroker loggingBroker;
 
-        public GroupOrchestrationService(IGroupProccessingService groupProccessingService)
+        public GroupOrchestrationService(
+            IGroupProccessingService groupProccessingService,
+            ILoggingBroker loggingBroker)
         {
             this.groupProccessingService = groupProccessingService;
+            this.loggingBroker = loggingBroker;
         }
 
-        public async ValueTask<Group> AddGroupAsync(Group group)
+        public ValueTask<Group> AddGroupAsync(Group group) =>
+        TryCatch(async () =>
         {
             return await this.groupProccessingService.AddGroupAsync(group);
-        }
+        });
 
         public IQueryable<Group> RetrieveAllGroups()
         {
