72dcbbbc73dab29e030ce1e5f5e5c9f565bf4640
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-10-31 21:17:22 +0500

    PROCCESSINGS: Add CreateAndLogProccessingValidationException

---
+++ Excel.Importer/Services/Proccessings/Applicants/ApplicantProccessingService.Exceptions.cs
@@ -0,0 +1,40 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Threading.Tasks;
+using Excel.Importer.Models.Foundations.Applicants;
+using Excel.Importer.Models.Foundations.Applicants.Exceptions;
+using Excel.Importer.Services.Proccessings.Groups.Exceptions;
+using Xeptions;
+
+namespace Excel.Importer.Services.Proccessings.Applicants
+{
+    public partial class ApplicantProccessingService
+    {
+        private delegate ValueTask<Applicant> ReturningApplicantFunction();
+
+        private async ValueTask<Applicant> TryCatch(ReturningApplicantFunction returningApplicantFunction)
+        {
+            try
+            {
+                return await returningApplicantFunction();
+            }
+            catch(ApplicantValidationException applicantValidationException)
+            {
+                throw CreateAndLogProccessingValidationException(applicantValidationException);
+            }
+        }
+
+        private ApplicantProccessingValidationException CreateAndLogProccessingValidationException(Xeption exception)
+        {
+            var applicantProccessingValidationException =
+                new ApplicantProccessingValidationException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(applicantProccessingValidationException);
+
+            return applicantProccessingValidationException;
+        }
+    }
+}
--- Excel.Importer/Services/Proccessings/Applicants/ApplicantProccessingService.cs
+++ Excel.Importer/Services/Proccessings/Applicants/ApplicantProccessingService.cs
@@ -4,18 +4,20 @@
 //===========================
 
 using System.Threading.Tasks;
+using Excel.Importer.Brokers.Loggings;
 using Excel.Importer.Models.Foundations.Applicants;
 using Excel.Importer.Services.Foundations.Applicants;
 
 namespace Excel.Importer.Services.Proccessings.Applicants
 {
-    public class ApplicantProccessingService : IApplicantProccessingService
+    public partial class ApplicantProccessingService : IApplicantProccessingService
     {
         private readonly IApplicantService applicantService;
-
+        private readonly ILoggingBroker loggingBroker;
         public ApplicantProccessingService(IApplicantService applicantService)
         {
             this.applicantService = applicantService;
+            this.loggingBroker = loggingBroker;
         }
 
         public ValueTask<Applicant> AddApplicantAsync(Applicant applicant)
