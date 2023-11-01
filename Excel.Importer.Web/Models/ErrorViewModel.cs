c5b1fe35612f4df24a85b3d92e2acd4ed9c34a07
99894 <99894@DESKTOP-K351R76>
2023-11-01 13:21:52 +0500

    INFRA: Initialize Web Project

---
+++ Excel.Importer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Excel.Importer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
