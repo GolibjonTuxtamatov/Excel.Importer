0fa10d589eab9cf8ad35d74c96099f81adc2f91d
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-11-01 21:18:57 +0500

    ORCHETRATIONS: Add CreateAndLogOrchetrationValidationException

---
+++ Excel.Importer/Services/Orchestrations/Applicants/ApplicantOrchestrationService.Exceptions.cs
@@ -0,0 +1,39 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Threading.Tasks;
+using Excel.Importer.Models.Foundations.Applicants;
+using Excel.Importer.Models.Foundations.Applicants.Exceptions;
+using Xeptions;
+
+namespace Excel.Importer.Services.Orchestrations.Applicants
+{
+    public partial class ApplicantOrchestrationService
+    {
+        private delegate ValueTask<Applicant> ReturningApplicantFunction();
+
+        private async ValueTask<Applicant> TryCatch(ReturningApplicantFunction returningApplicantFunction)
+        {
+            try
+            {
+                return await returningApplicantFunction();
+            }
+            catch (ApplicantProccessingValidationException applicantProccessingValidationException)
+            {
+                throw CreateAndLogOrchetrationValidationException(applicantProccessingValidationException);
+            }
+        }
+
+        private ApplicantOrchestrationValidationException CreateAndLogOrchetrationValidationException(Xeption exception)
+        {
+            var applicantOrchestrationValidationException =
+                new ApplicantOrchestrationValidationException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(applicantOrchestrationValidationException);
+
+            return applicantOrchestrationValidationException;
+        }
+    }
+}
--- Excel.Importer/Services/Orchestrations/Applicants/ApplicantOrchestrationService.cs
+++ Excel.Importer/Services/Orchestrations/Applicants/ApplicantOrchestrationService.cs
@@ -4,23 +4,27 @@
 //===========================
 
 using System.Threading.Tasks;
+using Excel.Importer.Brokers.Loggings;
 using Excel.Importer.Models.Foundations.Applicants;
 using Excel.Importer.Services.Proccessings.Applicants;
 
 namespace Excel.Importer.Services.Orchestrations.Applicants
 {
-    public class ApplicantOrchestrationService : IApplicantOrchestrationService
+    public partial class ApplicantOrchestrationService : IApplicantOrchestrationService
     {
         private readonly IApplicantProccessingService applicantProccessingService;
+        private readonly ILoggingBroker loggingBroker;
 
-        public ApplicantOrchestrationService(IApplicantProccessingService applicantProccessingService)
+        public ApplicantOrchestrationService(IApplicantProccessingService applicantProccessingService, ILoggingBroker loggingBroker)
         {
             this.applicantProccessingService = applicantProccessingService;
+            this.loggingBroker = loggingBroker;
         }
 
-        public async ValueTask<Applicant> AddApplicantAsync(Applicant applicant)
+        public ValueTask<Applicant> AddApplicantAsync(Applicant applicant) =>
+        TryCatch(async () =>
         {
             return await this.applicantProccessingService.AddApplicantAsync(applicant);
-        }
+        });
     }
 }
