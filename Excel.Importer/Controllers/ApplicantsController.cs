59dad35b091d5ebfbfbdb0f5fe4a585af0389299
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-11-02 11:22:56 +0500

    CONTROLLERS: Post Applicant

--- Excel.Importer/Controllers/ApplicantsController.cs
+++ Excel.Importer/Controllers/ApplicantsController.cs
@@ -5,6 +5,7 @@
 
 using System.Threading.Tasks;
 using Excel.Importer.Models.Foundations.Applicants;
+using Excel.Importer.Models.Foundations.Applicants.Exceptions;
 using Excel.Importer.Services.Orchestrations.Applicants;
 using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
@@ -15,7 +16,7 @@ namespace Excel.Importer.Controllers
     [Route("api/[controller]")]
     public class ApplicantsController : RESTFulController
     {
-        private IApplicantOrchestrationService applicantOrchestrationService;
+        private readonly IApplicantOrchestrationService applicantOrchestrationService;
 
         public ApplicantsController(IApplicantOrchestrationService applicantOrchestrationService)
         {
@@ -23,9 +24,35 @@ namespace Excel.Importer.Controllers
         }
 
         [HttpPost]
-        public async ValueTask<Applicant> PostGroupAsync(Applicant applicant)
+        public async ValueTask<ActionResult<Applicant>> PostApplicantAsync(Applicant applicant)
         {
-            return await this.applicantOrchestrationService.AddApplicantAsync(applicant);
+            try
+            {
+                Applicant postedApplicant = await this.applicantOrchestrationService.AddApplicantAsync(applicant);
+
+                return Created(postedApplicant);
+            }
+            catch (ApplicantOrchestrationValidationException applicantOrchestrationValidationException)
+            {
+                return BadRequest(applicantOrchestrationValidationException.InnerException);
+            }
+            catch (ApplicantOrchetrationDependencyValidationException applicantOrchetrationDependencyValidationException)
+                when (applicantOrchetrationDependencyValidationException.InnerException is AlreadyExistApplicantException)
+            {
+                return Conflict(applicantOrchetrationDependencyValidationException.InnerException);
+            }
+            catch (ApplicantOrchetrationDependencyValidationException applicantOrchetrationDependencyValidationException)
+            {
+                return Conflict(applicantOrchetrationDependencyValidationException.InnerException);
+            }
+            catch (ApplicantOrchestrationDependencyException applicantOrchestrationDependencyException)
+            {
+                return InternalServerError(applicantOrchestrationDependencyException.InnerException);
+            }
+            catch (ApplicantOrchestrationServiceException applicantOrchestrationServiceException)
+            {
+                return InternalServerError(applicantOrchestrationServiceException.InnerException);
+            }
         }
     }
 }
